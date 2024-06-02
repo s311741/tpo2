c47491559cc7236de01b97715fc0e5b7e5e72d9c
Ivan Sokolov <sokolovivanl2rwe@yandex.ru>
2024-06-02 20:45:53 +0300

    more tests

--- gentables.py
+++ gentables.py
@@ -1,15 +1,46 @@
-from math import *
-
-def gen(name, f):
+def gen(name, f, low, high, n):
     print(f'pub const {name} = [_]Entry {{')
-    size = 20
-    for i in range(size):
-        x = i * 2 * pi / size
+    step = (high - low) / n
+    for i in range(n):
+        x = low + i * step
         try:
             value = f(x)
-            if value > 1e10: continue
+            if abs(value) > 1e10: continue
             print(f'  Entry{{ .x = {x}, .y = {value} }},')
         except: continue
-    print('};')
+    print('};\n')
 
-gen("ln", log)
+from math import *
+def cot(x): return cos(x)/sin(x)
+def sec(x): return 1/cos(x)
+ln = log
+log_2 = log2
+def log_3(x): return ln(x)/ln(3)
+def log_5(x): return ln(x)/ln(5)
+log_10 = log10
+
+def f(x):
+    if x <= 0:
+        return (
+            (cos(x) * tan(x) - sin(x) - tan(x)/sec(x))**2
+            * (tan(x) / cos(x)) / (cot(x)**3)
+        )
+    else:
+        return (
+            ((log_3(x) / ln(x) + log_3(x)) ** 3
+             + (log_2(x) / log_3(x)) * log_10(x))
+            * (log_3(x) * ((log_3(x) - log_5(x)) * log_10(x))) **2
+        )
+
+print("pub const Entry = struct { x: f64, y: f64 };\n")
+gen("sin", sin, 0, 2*pi, 20)
+gen("cos", cos, 0, 2*pi, 20)
+gen("tan", tan, 0, 2*pi, 20)
+gen("cot", cot, 0, 2*pi, 20)
+gen("sec", sec, 0, 2*pi, 20)
+gen("ln", log, 0.001, 10, 20)
+gen("log_2", log_2, 0.001, 10, 20)
+gen("log_3", log_3, 0.001, 10, 20)
+gen("log_5", log_5, 0.001, 10, 20)
+gen("log_10", log_10, 0.001, 10, 20)
+gen("f", f, -10, 10, 100)
--- src/math.zig
+++ src/math.zig
@@ -4,9 +4,9 @@ const ln_ = std.math.log;
 const pi = std.math.pi;
 
 fn sin(x: f64) f64 { return sin_(x); }
-fn cos(x: f64) f64 { return sin(x + 0.5 * pi); }
 fn ln(x: f64) f64 { return @log(x); }
 
+fn cos(x: f64) f64 { return sin(x + 0.5 * pi); }
 fn tan(x: f64) f64 { return sin(x) / cos(x); }
 fn cot(x: f64) f64 { return cos(x) / sin(x); }
 fn sec(x: f64) f64 { return 1 / cos(x); }
@@ -58,35 +58,60 @@ fn expectMatchTable(
   }
 }
 
-test "tables" {
-  const eps = 0.001;
-
-  // Trig
-  try expectMatchTable(sin, &tables.sin, eps);
-  try expectMatchTable(cos, &tables.cos, eps);
-  try expectMatchTable(tan, &tables.tan, eps);
-  try expectMatchTable(cot, &tables.cot, eps);
-  try expectMatchTable(sec, &tables.sec, eps);
-
-  // Log
-  try expectMatchTable(ln, &tables.ln, eps);
-
-  // F
-  try expectMatchTable(f, &tables.f, eps);
-}
-
-test "negative is periodic" {
-  for (1..10) |i| {
-    const initialX = -0.1 * @as(f64, @floatFromInt(i));
-    const initialValue = f(initialX);
-    for (1..10) |j| {
-      const x = initialX - 2 * pi * @as(f64, @floatFromInt(j));
-      try expectWithinEps(f(x), initialValue, 0.001);
+fn expectPeriodic(
+  fun: fn (f64) f64,
+  period: f64,
+  start: f64,
+  stepsWithinPeriod: usize,
+  periods: usize,
+  eps: f64,
+) !void {
+  const step = period / @as(f64, @floatFromInt(stepsWithinPeriod));
+  for (0..stepsWithinPeriod) |i| {
+    const initialX = start + step * @as(f64, @floatFromInt(i));
+    const initialValue = fun(initialX);
+    for (1..periods) |j| {
+      const x = initialX + period * @as(f64, @floatFromInt(j));
+      try expectWithinEps(fun(x), initialValue, eps);
     }
   }
 }
 
-test "nans" {
+test "tables" {
+  const eps = 0.001;
+
+  {
+    try expectMatchTable(sin, &tables.sin, eps);
+    try expectMatchTable(cos, &tables.cos, eps);
+    try expectMatchTable(tan, &tables.tan, eps);
+    try expectMatchTable(cot, &tables.cot, eps);
+    try expectMatchTable(sec, &tables.sec, eps);
+  }
+
+  {
+    try expectMatchTable(ln, &tables.ln, eps);
+    try expectMatchTable(log_2, &tables.log_2, eps);
+    try expectMatchTable(log_3, &tables.log_3, eps);
+    try expectMatchTable(log_5, &tables.log_5, eps);
+    try expectMatchTable(log_10, &tables.log_10, eps);
+  }
+
+  try expectMatchTable(f, &tables.f, eps);
+}
+
+test "periodic" {
+  const steps = 10;
+  const periods = 100;
+  const eps = 0.001;
+  try expectPeriodic(f,  -2*pi, 0, steps, periods, eps);
+  try expectPeriodic(sin, 2*pi, 0, steps, periods, eps);
+  try expectPeriodic(cos, 2*pi, 0, steps, periods, eps);
+  try expectPeriodic(tan, 2*pi, 0, steps, periods, eps);
+  try expectPeriodic(cot, 2*pi, 1e-3, steps, periods, eps);
+  try expectPeriodic(sec, 2*pi, 0, steps, periods, eps);
+}
+
+test "poles" {
   for (1..10) |i| {
     const value = f(pi * (0.5 - @as(f64, @floatFromInt(i))));
     try t.expect(std.math.isNan(value) or @abs(value) > 1e10);
